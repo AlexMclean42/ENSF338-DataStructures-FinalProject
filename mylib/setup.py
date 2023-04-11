c6605254aa3da07fb85181d468d80055aea9a458
Alex MCLEAN <alexander.mclean@ucalgary.ca>
2023-04-11 16:46:36 -0600

    commit

--- mylib/setup.py
+++ mylib/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='mylib',
+setup(name='Datastructures_Alex-Kaylyn',
     version='1.0',
-    description='A Singly Linked List Data structure',
+    description='Data structures, linked lists, treestructures',
     author='Alex McLean, Kaylyn Tanton',
     author_email='alexander.mclean@ucalgary.ca, kaylyn.tanton@ucalgary.ca ',
-    packages=['mylib'],
+    packages=['datastructures'],
     install_requires=[],
     )
